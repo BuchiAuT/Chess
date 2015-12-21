c3e4bfb5dd195fe7e9dfde34fc941c3345ce4526
Tobias <Tobias@Tobias-PC>
2015-12-21 09:25:48 +0100

    stimmt no nit

--- Chess/src/org/ultimate/chess/model/Dame.java
+++ Chess/src/org/ultimate/chess/model/Dame.java
@@ -1,42 +1,82 @@
 package org.ultimate.chess.model;
 
-/**
- * 
- * @author Michael Krapf
- *
- */
-
 public class Dame extends Figur
-{
-	public Dame()
+{	
+	public Dame(Position pos, String name)
 	{
-		super();
+		super(pos,name);
 	}
 	
-	public boolean spielZug(SpielFeld sp, Position nach)
+	public boolean spielZug(SpielFeld spielfeld, Position position)
 	{
-		boolean statement = false;
-		byte spielfeldendeX = 7;
-		byte spielfeldendeY = 7;
+		boolean statement=false;
 		
-		if(((nach.x) == (this.pos.x+1)) && ((nach.y) == (this.pos.y+1)))
+		if(spielzugMoeglich(spielfeld,position))
 		{
-			for(int i = 0; i < sp.figuren.length; i++)
-			{
-				if(((sp.holeFigur().pos.x!=nach.x) && (sp.holeFigur().pos.y!=nach.y)))
-				{
-					if(((nach.x <= spielfeldendeX) || (nach.x >= 0)) || ((nach.y >= spielfeldendeY) || (nach.y >= 0)))
-					{
-						statement = true;
-					}
-				}
-			}
+			statement=true;
+			this.pos.setX(position.getX());
+			this.pos.setY(position.getY());
 		}
+				
 		return statement;
 	}
 	
-	public boolean spielzugMoeglich(SpielFeld sp, Position nach)
+	public boolean spielzugMoeglich(SpielFeld spielfeld, Position position)
 	{
+		boolean statement=false;
+		byte spielfeldendeX=7;
+		byte spielfeldendeY=7;
 		
+		// Diagonal schlagen	(x+1, y+1)
+		
+		
+		if((((position.getX())==(this.pos.getX()+1)) && ((position.getY())==(this.pos.getY()+1))) || (((position.getX())==(this.pos.getX()-1)) && ((position.getY())==(this.pos.getY()+1))))	
+		{	
+				if((((spielfeld.holeFigur(pos).pos.getX()==position.getX()) && (spielfeld.holeFigur(pos).pos.getY())==position.getY())) && (spielfeld.holeFigur(pos).getFarbe()!=this.getFarbe())) 	// Abfragen ob eine gegnerische Figur auf dem Feld steht (andere Farbe)
+				{
+					if((position.getX()<=spielfeldendeX)&&(position.getX()>=0)&&(position.getY()<=spielfeldendeY)&&(position.getY()>=0)) // Abfragen ob Bewegung nicht au�erhalb des Feldes geht
+					{
+						spielfeld.figuren.remove(spielfeld.holeFigur(pos));		// Figur vom Spielfeld entfernen
+						
+						statement=true;
+					}
+				}
+		}
+		
+//------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------		
+		
+		// 1 Feld nach vorne	
+		
+		if((((position.getX())==(this.pos.getX()+1)) && ((position.getY())==(this.pos.getY()-1))) || (((position.getX())==(this.pos.getX()-1)) && ((position.getY())==(this.pos.getY()+1))))	
+		{	
+				if((((spielfeld.holeFigur(pos).pos.getX()==position.getX()) && (spielfeld.holeFigur(pos).pos.getY())==position.getY())) && (spielfeld.holeFigur(pos).getFarbe()!=this.getFarbe())) 	// Abfragen ob eine gegnerische Figur auf dem Feld steht (andere Farbe)
+				{
+					if((position.getX()<=spielfeldendeX)&&(position.getX()>=0)&&(position.getY()<=spielfeldendeY)&&(position.getY()>=0)) // Abfragen ob Bewegung nicht au�erhalb des Feldes geht
+					{
+						spielfeld.figuren.remove(spielfeld.holeFigur(pos));		// Figur vom Spielfeld entfernen
+						
+						statement=true;
+					}
+				}
+		}
+		
+//------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+		
+		// 2 gerade nach Vorne bewegen, kann nur gemacht werden wenn y=1 (Y-Position des Bauern zu Beginn des Spiels)	
+		
+		if((((position.getX())==(this.pos.getX()+1)) && ((position.getY())==(this.pos.getY()+1))) || (((position.getX())==(this.pos.getX()-1)) && ((position.getY())==(this.pos.getY()+1))))	
+		{	
+				if((((spielfeld.holeFigur(pos).pos.getX()==position.getX()) && (spielfeld.holeFigur(pos).pos.getY())==position.getY())) && (spielfeld.holeFigur(pos).getFarbe()!=this.getFarbe())) 	// Abfragen ob eine gegnerische Figur auf dem Feld steht (andere Farbe)
+				{
+					if((position.getX()<=spielfeldendeX)&&(position.getX()>=0)&&(position.getY()<=spielfeldendeY)&&(position.getY()>=0)) // Abfragen ob Bewegung nicht au�erhalb des Feldes geht
+					{
+						spielfeld.figuren.remove(spielfeld.holeFigur(pos));		// Figur vom Spielfeld entfernen
+						
+						statement=true;
+					}
+				}
+		}
+		
+		return statement;
 	}
 }
--- Chess/src/org/ultimate/chess/model/Laeufer.java
+++ Chess/src/org/ultimate/chess/model/Laeufer.java
@@ -1,82 +1,81 @@
 package org.ultimate.chess.model;
-<<<<<<< HEAD
-=======
-
->>>>>>> branch 'master' of https://github.com/BuchiAuT/Chess
-/**
- * 
- * @author Michael Krapf
- *
- */
 
 public class Laeufer extends Figur
-{
-	public Laeufer()
+{	
+	public Laeufer(Position pos, String name)
 	{
-		super();
+		super(pos,name);
 	}
 	
-	public boolean spielZug(SpielFeld sp, Position nach)
+	public boolean spielZug(SpielFeld spielfeld, Position position)
 	{
-		boolean statement = false;
-		byte spielfeldendeX = 7;
-		byte spielfeldendeY = 7;
+		boolean statement=false;
 		
-		if(((nach.x) == (this.pos.x+1)) && ((nach.y) == (this.pos.y+1))) // nach rechts und nach oben
+		if(spielzugMoeglich(spielfeld,position))
 		{
-			if(((sp.holeFigur().pos.x!=nach.x) && (sp.figuren[i].pos.y!=nach.y)))
-			{
-				if(((nach.x <= spielfeldendeX) || (nach.x >= 0)) || ((nach.y >= spielfeldendeY) || (nach.y >= 0)))
-				{
-						statement = true;
-				}
-			}
+			statement=true;
+			this.pos.setX(position.getX());
+			this.pos.setY(position.getY());
 		}
-		
-		// -----------------------------------------------------------------------------------------------
-		
-		if(((nach.x) == (this.pos.x-1)) && ((nach.y) == (this.pos.y+1))) // nach links und nach oben
-		{
-			if(((sp.holeFigur().pos.x!=nach.x) && (sp.holeFigur().pos.y!=nach.y)))
-			{
-				if(((nach.x <= spielfeldendeX) || (nach.x >= 0)) || ((nach.y >= spielfeldendeY) || (nach.y >= 0)))
-				{
-					statement = true;
-				}
-			}
-		}
-		
-		// -----------------------------------------------------------------------------------------------
-		
-		if(((nach.x) == (this.pos.x-1)) && ((nach.y) == (this.pos.y-1))) // nach links und nach unten
-		{
-			if(((sp.holeFigur().pos.x!=nach.x) && (sp.holeFigur().pos.y!=nach.y)))
-			{
-				if(((nach.x <= spielfeldendeX) || (nach.x >= 0)) || ((nach.y >= spielfeldendeY) || (nach.y >= 0)))
-				{
-					statement = true;
-				}
-			}
-		}
-		
-		// -----------------------------------------------------------------------------------------------
-		
-		if(((nach.x) == (this.pos.x+1)) && ((nach.y) == (this.pos.y-1))) // nach rechts und nach unten
-		{
-			if(((sp.holeFigur().pos.x!=nach.x) && (sp.holeFigur().pos.y!=nach.y)))
-			{
-				if(((nach.x <= spielfeldendeX) || (nach.x >= 0)) || ((nach.y >= spielfeldendeY) || (nach.y >= 0)))
-				{
-					statement = true;
-				}
-			}
-		}
-		
+				
 		return statement;
 	}
 	
-	public boolean spielzugMoeglich(SpielFeld sp, Position nach)
+	public boolean spielzugMoeglich(SpielFeld spielfeld, Position position)
 	{
-		return spielZug(sp, nach);
+		boolean statement=false;
+		byte spielfeldendeX=7;
+		byte spielfeldendeY=7;
+		
+		// Diagonal schlagen	(rechts vor, links vor)
+		
+		
+		if((((position.getX())==(this.pos.getX()+1)) && ((position.getY())==(this.pos.getY()+1))) || (((position.getX())==(this.pos.getX()-1)) && ((position.getY())==(this.pos.getY()+1))))	
+		{	
+				if((((spielfeld.holeFigur(pos).pos.getX()==position.getX()) && (spielfeld.holeFigur(pos).pos.getY())==position.getY())) && (spielfeld.holeFigur(pos).getFarbe()!=this.getFarbe())) 	// Abfragen ob eine gegnerische Figur auf dem Feld steht (andere Farbe)
+				{
+					if((position.getX()<=spielfeldendeX)&&(position.getX()>=0)&&(position.getY()<=spielfeldendeY)&&(position.getY()>=0)) // Abfragen ob Bewegung nicht au�erhalb des Feldes geht
+					{
+						spielfeld.figuren.remove(spielfeld.holeFigur(pos));		// Figur vom Spielfeld entfernen
+						
+						statement=true;
+					}
+				}
+		}
+		
+//------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------		
+		
+		// 1 Feld nach vorne	
+		
+		if(((position.getX())==(this.pos.getX())) && ((position.getY())==(this.pos.getY()+1))) 
+		{
+				if(((spielfeld.holeFigur(pos).pos.getX()!=position.getX()) && (spielfeld.holeFigur(pos).pos.getY()!=position.getY()))) // Abfragen dass keine Figur auf dem Feld steht 
+				{					
+					if((position.getX()<=spielfeldendeX)&&(position.getX()>=0)&&(position.getY()<=spielfeldendeY)&&(position.getY()>=0)) // Abfragen ob Bewegung nicht au�erhalb des Feldes geht, da Bauern nur Diagonal schlagen koennen						
+					{
+							statement=true;
+					}
+				}
+		}
+		
+//------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+		
+		// 2 gerade nach Vorne bewegen, kann nur gemacht werden wenn y=1 (Y-Position des Bauern zu Beginn des Spiels)	
+		
+		if(((position.getX())==(this.pos.getX())) && ((position.getY())==(this.pos.getY())+2))
+		{
+			if(((spielfeld.holeFigur(pos).pos.getX()!=position.getX()) && (spielfeld.holeFigur(pos).pos.getY()!=position.getY()))) // Abfragen ob generell keine Figur auf dem Feld steht 
+				{
+					if((position.getX()<=spielfeldendeX)&&(position.getX()>=0)&&(position.getY()<=spielfeldendeY)&&(position.getY()>=0)) // Abfragen ob Bewegung nicht au�erhalb des Feldes geht
+					{
+						if(this.pos.getY()==1)	//Bauer steht an Startposition
+						{
+							statement=true;
+						}
+					}
+				}
+		}
+		
+		return statement;
 	}
 }
