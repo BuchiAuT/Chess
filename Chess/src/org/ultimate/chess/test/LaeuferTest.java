6a34a09c87b0bb124eed9bc880c24c4c8d87f073
Michael Krapf <Michael Krapf@Michael-Laptop>
2016-01-11 08:47:45 +0100

    test klassen

---
+++ Chess/src/org/ultimate/chess/test/LaeuferTest.java
@@ -0,0 +1,50 @@
+/**
+ * 
+ * @author Michael Krapf
+ * 
+ */
+
+package org.ultimate.chess.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class LaeuferTest {
+
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void testSpielZug() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSpielzugMoeglich() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testLaeufer() {
+		fail("Not yet implemented");
+	}
+
+}
