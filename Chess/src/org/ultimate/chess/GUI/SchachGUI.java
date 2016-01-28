c0a850c7856b648de25d16363b267b8085904d23
Bernhard <Bernhard@Buchi>
2016-01-28 13:32:04 +0100

    jooo

---
+++ Chess/src/org/ultimate/chess/GUI/SchachGUI.java
@@ -0,0 +1,13 @@
+package org.ultimate.chess.GUI;
+
+import org.ultimate.chess.model.SpielFeld;
+
+public class SchachGUI 
+{
+	private SpielFeld sf;
+	public SchachGUI(SpielFeld sf)
+	{
+		this.sf = sf;
+	}
+
+}
