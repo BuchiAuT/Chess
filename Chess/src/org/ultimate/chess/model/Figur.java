c5f2955e62ec07a9a41c1a702557edfd2316aeac
Tobias <Tobias@Tobias-PC>
2016-01-28 12:30:18 +0100

    testsfertig

--- Chess/src/org/ultimate/chess/model/Figur.java
+++ Chess/src/org/ultimate/chess/model/Figur.java
@@ -5,24 +5,29 @@ abstract class Figur
 	Position pos;
 	private String name;
 	boolean farbeWeiss; //true = weiﬂ
+	
 	public Figur(Position pos,boolean farbeWeiss)
 	{	
 		this.pos = pos;
 		this.farbeWeiss = farbeWeiss;
 		
 	}
+	
 	public boolean getFarbe()
 	{
 		return farbeWeiss;
 	}
+	
 	public String getName()
 	{
 		return name;
 	}
+	
 	public Position getPos()
 	{
 		return pos;
 	}
+	
 @Override
 public String toString() 
 {
@@ -35,6 +40,7 @@ public String toString()
 	return name.charAt(1)+",S";
 	}
 }
+
 	public boolean spielZug(SpielFeld sp,Position nach)
 	{
 		boolean statement=false;
